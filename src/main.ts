b1acd09d9225aa488dd04892c86c6b29c2f81ce1
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2024-10-16 10:53:36 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const buttonName = "Bust Bug 🪳";
+const button = document.createElement("button");
+button.innerHTML = buttonName;
+app.append(button);
