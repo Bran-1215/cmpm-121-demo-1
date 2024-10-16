2e311888e8819b6c44dc5d1f9274be938fbbf3ae
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2024-10-16 11:22:09 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,12 @@ const buttonName = "Bust Bug 🪳";
 const button = document.createElement("button");
 button.innerHTML = buttonName;
 app.append(button);
+
+let counterVal:number = 0;
+const counterDiv = document.createElement("div");
+counterDiv.innerHTML = "🪳 Busted: " + counterVal;
+button.addEventListener("click", () => {
+    console.log(counterVal);
+    counterDiv.innerHTML = "🪳Busted: " + ++counterVal;
+});
+app.append(counterDiv);
