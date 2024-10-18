c154e587ae387d6dbf1deb8c2f23f62245f36912
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2024-10-18 13:13:04 -0700

    Step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -30,28 +30,32 @@ let itemCCounter = 0;
 const statusDisplay = document.createElement("div");
 statusDisplay.innerHTML = `Busting Rate: ${rate} bugs/second, Item A: ${itemACounter}, Item B: ${itemBCounter}, Item C: ${itemCCounter}`;
 document.addEventListener("click", () => {
-  statusDisplay.innerHTML = `Busting Rate: ${Math.round(rate*10)/10} bugs/second, Item A: ${itemACounter}, Item B: ${itemBCounter}, Item C: ${itemCCounter}`;
+  statusDisplay.innerHTML = `Busting Rate: ${Math.round(rate * 10) / 10} bugs/second, Item A: ${itemACounter}, Item B: ${itemBCounter}, Item C: ${itemCCounter}`;
 });
 app.append(statusDisplay);
 
 // Upgrade Button A
-const upgradeButtonAName = "Upgrade: Auto-Buster A (10 bugs)";
+let upgradeACost = 10;
 const upgradeButtonA = document.createElement("button");
-upgradeButtonA.innerHTML = upgradeButtonAName;
+upgradeButtonA.innerHTML = `Upgrade: Auto-Buster A (${upgradeACost} bugs)`;
+document.addEventListener("click", () => {
+  upgradeButtonA.innerHTML = `Upgrade: Auto-Buster A (${upgradeACost} bugs)`;
+});
 app.append(upgradeButtonA);
 
 document.addEventListener("input", () => {
-  upgradeButtonA.disabled = counterVal < 10;
+  upgradeButtonA.disabled = counterVal < upgradeACost;
 });
 
 upgradeButtonA.addEventListener("click", () => {
-  if (counterVal >= 10) {
+  if (counterVal >= upgradeACost) {
     rate += 0.1;
     itemACounter += 1;
-    counterVal -= 10;
+    counterVal -= upgradeACost;
+    upgradeACost = Math.round((upgradeACost * 1.15)*100)/100;
     let timestampA = performance.now();
     requestAnimationFrame(function step(tA) {
-      counterVal += ((tA - timestampA) / 1000)*0.1;
+      counterVal += ((tA - timestampA) / 1000) * 0.1;
       timestampA = tA;
       counterDiv.innerHTML = "🪳Busted: " + Math.floor(counterVal);
       requestAnimationFrame(step);
@@ -60,23 +64,27 @@ upgradeButtonA.addEventListener("click", () => {
 });
 
 // Upgrade Button B
-const upgradeButtonBName = "Upgrade: Auto-Buster B (100 bugs)";
+let upgradeBCost = 100;
 const upgradeButtonB = document.createElement("button");
-upgradeButtonB.innerHTML = upgradeButtonBName;
+upgradeButtonB.innerHTML = `Upgrade: Auto-Buster B (${upgradeBCost} bugs)`;
+document.addEventListener("click", () => {
+  upgradeButtonB.innerHTML = `Upgrade: Auto-Buster B (${upgradeBCost} bugs)`;
+});
 app.append(upgradeButtonB);
 
 document.addEventListener("input", () => {
-  upgradeButtonB.disabled = counterVal < 100;
+  upgradeButtonB.disabled = counterVal < upgradeBCost;
 });
 
 upgradeButtonB.addEventListener("click", () => {
-  if (counterVal >= 100) {
+  if (counterVal >= upgradeBCost) {
     rate += 2;
     itemBCounter += 1;
-    counterVal -= 100;
+    counterVal -= upgradeBCost;
+    upgradeBCost = Math.round((upgradeBCost * 1.15)*100)/100;
     let timestampB = performance.now();
     requestAnimationFrame(function step(tB) {
-      counterVal += ((tB - timestampB) / 1000)*2;
+      counterVal += ((tB - timestampB) / 1000) * 2;
       timestampB = tB;
       counterDiv.innerHTML = "🪳Busted: " + Math.floor(counterVal);
       requestAnimationFrame(step);
@@ -85,27 +93,30 @@ upgradeButtonB.addEventListener("click", () => {
 });
 
 // Upgrade Button C
-const upgradeButtonCName = "Upgrade: Auto-Buster C (1000 bugs)";
+let upgradeCCost = 1000;
 const upgradeButtonC = document.createElement("button");
-upgradeButtonC.innerHTML = upgradeButtonCName;
+upgradeButtonC.innerHTML = `Upgrade: Auto-Buster C (${upgradeCCost} bugs)`;
+document.addEventListener("click", () => {
+  upgradeButtonC.innerHTML = `Upgrade: Auto-Buster C (${upgradeCCost} bugs)`;
+});
 app.append(upgradeButtonC);
 
 document.addEventListener("input", () => {
-  upgradeButtonC.disabled = counterVal < 1000;
+  upgradeButtonC.disabled = counterVal < upgradeCCost;
 });
 
 upgradeButtonC.addEventListener("click", () => {
-  if (counterVal >= 1000) {
+  if (counterVal >= upgradeCCost) {
     rate += 50;
     itemCCounter += 1;
-    counterVal -= 1000;
+    counterVal -= upgradeCCost;
+    upgradeCCost = Math.round((upgradeCCost * 1.15)*100)/100;
     let timestampC = performance.now();
     requestAnimationFrame(function step(tC) {
-      counterVal += ((tC - timestampC) / 1000)*50;
+      counterVal += ((tC - timestampC) / 1000) * 50;
       timestampC = tC;
       counterDiv.innerHTML = "🪳Busted: " + Math.floor(counterVal);
       requestAnimationFrame(step);
     });
   }
 });
-
