6a5e33c56e9940bf11879d4939cd1d19127e5b92
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2024-10-18 11:59:57 -0700

    Step 6  complete

--- src/main.ts
+++ src/main.ts
@@ -22,23 +22,90 @@ button.addEventListener("click", () => {
 });
 app.append(counterDiv);
 
-const upgradeButtonName = "Upgrade: Auto-Buster (10 bugs)";
-const upgradeButton = document.createElement("button");
-upgradeButton.innerHTML = upgradeButtonName;
-app.append(upgradeButton);
+// Status Display
+let rate = 0;
+let itemACounter = 0;
+let itemBCounter = 0;
+let itemCCounter = 0;
+const statusDisplay = document.createElement("div");
+statusDisplay.innerHTML = `Busting Rate: ${rate} bugs/second, Item A: ${itemACounter}, Item B: ${itemBCounter}, Item C: ${itemCCounter}`;
+document.addEventListener("click", () => {
+  statusDisplay.innerHTML = `Busting Rate: ${Math.round(rate*10)/10} bugs/second, Item A: ${itemACounter}, Item B: ${itemBCounter}, Item C: ${itemCCounter}`;
+});
+app.append(statusDisplay);
+
+// Upgrade Button A
+const upgradeButtonAName = "Upgrade: Auto-Buster A (10 bugs)";
+const upgradeButtonA = document.createElement("button");
+upgradeButtonA.innerHTML = upgradeButtonAName;
+app.append(upgradeButtonA);
 
 document.addEventListener("input", () => {
-  upgradeButton.disabled = counterVal < 10;
+  upgradeButtonA.disabled = counterVal < 10;
 });
-upgradeButton.addEventListener("click", () => {
+
+upgradeButtonA.addEventListener("click", () => {
   if (counterVal >= 10) {
+    rate += 0.1;
+    itemACounter += 1;
     counterVal -= 10;
-    let timestamp = performance.now();
-    requestAnimationFrame(function step(t) {
-      counterVal += (t - timestamp) / 1000;
-      timestamp = t;
+    let timestampA = performance.now();
+    requestAnimationFrame(function step(tA) {
+      counterVal += ((tA - timestampA) / 1000)*0.1;
+      timestampA = tA;
       counterDiv.innerHTML = "🪳Busted: " + Math.floor(counterVal);
       requestAnimationFrame(step);
     });
   }
 });
+
+// Upgrade Button B
+const upgradeButtonBName = "Upgrade: Auto-Buster B (100 bugs)";
+const upgradeButtonB = document.createElement("button");
+upgradeButtonB.innerHTML = upgradeButtonBName;
+app.append(upgradeButtonB);
+
+document.addEventListener("input", () => {
+  upgradeButtonB.disabled = counterVal < 100;
+});
+
+upgradeButtonB.addEventListener("click", () => {
+  if (counterVal >= 100) {
+    rate += 2;
+    itemBCounter += 1;
+    counterVal -= 100;
+    let timestampB = performance.now();
+    requestAnimationFrame(function step(tB) {
+      counterVal += ((tB - timestampB) / 1000)*2;
+      timestampB = tB;
+      counterDiv.innerHTML = "🪳Busted: " + Math.floor(counterVal);
+      requestAnimationFrame(step);
+    });
+  }
+});
+
+// Upgrade Button C
+const upgradeButtonCName = "Upgrade: Auto-Buster C (1000 bugs)";
+const upgradeButtonC = document.createElement("button");
+upgradeButtonC.innerHTML = upgradeButtonCName;
+app.append(upgradeButtonC);
+
+document.addEventListener("input", () => {
+  upgradeButtonC.disabled = counterVal < 1000;
+});
+
+upgradeButtonC.addEventListener("click", () => {
+  if (counterVal >= 1000) {
+    rate += 50;
+    itemCCounter += 1;
+    counterVal -= 1000;
+    let timestampC = performance.now();
+    requestAnimationFrame(function step(tC) {
+      counterVal += ((tC - timestampC) / 1000)*50;
+      timestampC = tC;
+      counterDiv.innerHTML = "🪳Busted: " + Math.floor(counterVal);
+      requestAnimationFrame(step);
+    });
+  }
+});
+
